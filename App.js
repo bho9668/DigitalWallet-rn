024f8063c3fbe560a21b47c85c8c4f3c4fce2388
doomworld <kingsmartin1@gmail.com>
2021-01-29 00:04:47 +0530

    A react native project on digital wallet with only ui focused with home signup and scan screen and some assets including fonts images icons are added and signup page is fully build but no testing some error is occured not fixed ready to move on to next page and fix error

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+
+import { SignUp } from './Screens';
+
+// theme for application
+const theme = {
+	...DefaultTheme,
+	colors: {
+		...DefaultTheme.colors,
+		border: 'transparent',
+	},
+};
+
+// create stack navigators
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer theme={theme}>
+			<Stack.Navigator
+				screenOptions={{
+					headerShow: false,
+				}}
+				initialRouteName={'SignUp'}
+			>
+				<Stack.Screen name="SignUp" component={SignUp} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
